fb10462fbcb6001d4517c7a06f1aaa75a1fc93dd
hardkornate <hardkornate@gmail.com>
2017-07-23 09:39:23 -0600

    submission 2

--- app/src/main/java/com/example/android/habittracker/MainActivity.java
+++ app/src/main/java/com/example/android/habittracker/MainActivity.java
@@ -1,16 +1,12 @@
 package com.example.android.habittracker;
 
-import android.content.ContentValues;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.example.android.habittracker.data.HabitContract.HabitEntry;
 import com.example.android.habittracker.data.HabitDbHelper;
-
-import static com.example.android.habittracker.data.HabitContract.HabitEntry.TABLE_NAME;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,84 +25,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
 
         super.onStart();
-        insertHabits();
-        displayDatabaseInfo();
-
-    }
-
-    private void insertHabits(){
-
-        // Gets the database in write mode
-        SQLiteDatabase db = mDbHelper.getWritableDatabase();
-
-        // Deletes all data from the database
-        db.delete(TABLE_NAME, null, null);
-
-        // Create a ContentValues object where column names are the keys,
-        // and habits are the values.
-        ContentValues values = new ContentValues();
-
-        values.put(HabitEntry.COLUMN_HABIT, "Smoking");
-        values.put(HabitEntry.COLUMN_COUNT, 41);
-        values.put(HabitEntry.COLUMN_DAYS_TRACKED, 3);
-        values.put(HabitEntry.COLUMN_TIMES_PER_DAY, (getTimesPerDay(41, 3)));
-
-        db.insert(TABLE_NAME, null, values);
-
-        values.put(HabitEntry.COLUMN_HABIT, "Eating");
-        values.put(HabitEntry.COLUMN_COUNT, 9);
-        values.put(HabitEntry.COLUMN_DAYS_TRACKED, 3);
-        values.put(HabitEntry.COLUMN_TIMES_PER_DAY, (getTimesPerDay(9, 3)));
-
-        db.insert(TABLE_NAME, null, values);
-
-        values.put(HabitEntry.COLUMN_HABIT, "Sleeping");
-        values.put(HabitEntry.COLUMN_COUNT, 3);
-        values.put(HabitEntry.COLUMN_DAYS_TRACKED, 3);
-        values.put(HabitEntry.COLUMN_TIMES_PER_DAY, (getTimesPerDay(3, 3)));
-
-        db.insert(TABLE_NAME, null, values);
-
-    }
-
-    public static long getTimesPerDay(int days, int count){
-
-        return(days/count);
-
-    }
-
-    /**
-     * Temporary helper method to display information in the onscreen TextView about the state of
-     * the pets database.
-     */
-
-    private void displayDatabaseInfo() {
-
-        // Create and/or open a database to read from it
-        SQLiteDatabase db = mDbHelper.getReadableDatabase();
-
-        // Define a projection that specifies which columns from the database
-        // you will actually use after this query.
-        String[] projection = {
-                HabitEntry._ID,
-                HabitEntry.COLUMN_HABIT,
-                HabitEntry.COLUMN_COUNT,
-                HabitEntry.COLUMN_DAYS_TRACKED,
-                HabitEntry.COLUMN_TIMES_PER_DAY};
-
-        // Perform a query on the pets table
-        Cursor cursor = db.query(
-                TABLE_NAME,   // The table to query
-                projection,            // The columns to return
-                null,                  // The columns for the WHERE clause
-                null,                  // The values for the WHERE clause
-                null,                  // Don't group the rows
-                null,                  // Don't filter by row groups
-                null);                   // The sort order
+        mDbHelper = HabitDbHelper.insertHabits(mDbHelper);
 
         TextView displayView = (TextView) findViewById(R.id.text_view_habit);
 
-        try {
+        try (Cursor cursor = HabitDbHelper.displayDatabaseInfo(mDbHelper)) {
             // Create a header in the Text View that looks like this:
             //
             // The pets table contains <number of rows in Cursor> habits.
@@ -146,12 +69,11 @@ public class MainActivity extends AppCompatActivity {
 
             }
 
-        } finally {
-
-            // Always close the cursor when you're done reading from it. This releases all its
-            // resources and makes it invalid.
-            cursor.close();
         }
+
+        // Always close the cursor when you're done reading from it. This releases all its
+        // resources and makes it invalid.
+
 
     }
 
--- app/src/main/java/com/example/android/habittracker/data/HabitDbHelper.java
+++ app/src/main/java/com/example/android/habittracker/data/HabitDbHelper.java
@@ -1,16 +1,14 @@
 package com.example.android.habittracker.data;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import com.example.android.habittracker.data.HabitContract.HabitEntry;
 
 import static com.example.android.habittracker.data.HabitContract.HabitEntry.TABLE_NAME;
-
-/**
- * Created by Hardkornate on 7/19/17.
- */
 
 public class HabitDbHelper extends SQLiteOpenHelper {
 
@@ -31,6 +29,78 @@ public class HabitDbHelper extends SQLiteOpenHelper {
      */
     public HabitDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public static HabitDbHelper insertHabits(HabitDbHelper mDbHelper) {
+
+        // Gets the database in write mode
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        // Deletes all data from the database
+        db.delete(TABLE_NAME, null, null);
+
+        // Create a ContentValues object where column names are the keys,
+        // and habits are the values.
+        ContentValues values = new ContentValues();
+
+        values.put(HabitEntry.COLUMN_HABIT, "Smoking");
+        values.put(HabitEntry.COLUMN_COUNT, 41);
+        values.put(HabitEntry.COLUMN_DAYS_TRACKED, 3);
+        values.put(HabitEntry.COLUMN_TIMES_PER_DAY, (getTimesPerDay(41, 3)));
+
+        db.insert(TABLE_NAME, null, values);
+
+        values.put(HabitEntry.COLUMN_HABIT, "Eating");
+        values.put(HabitEntry.COLUMN_COUNT, 9);
+        values.put(HabitEntry.COLUMN_DAYS_TRACKED, 3);
+        values.put(HabitEntry.COLUMN_TIMES_PER_DAY, (getTimesPerDay(9, 3)));
+
+        db.insert(TABLE_NAME, null, values);
+
+        values.put(HabitEntry.COLUMN_HABIT, "Sleeping");
+        values.put(HabitEntry.COLUMN_COUNT, 3);
+        values.put(HabitEntry.COLUMN_DAYS_TRACKED, 3);
+        values.put(HabitEntry.COLUMN_TIMES_PER_DAY, (getTimesPerDay(3, 3)));
+
+        db.insert(TABLE_NAME, null, values);
+
+        return mDbHelper;
+
+    }
+
+    private static long getTimesPerDay(int days, int count) {
+
+        return (days / count);
+
+    }
+
+    /**
+     * Temporary helper method to display information in the onscreen TextView about the state of
+     * the pets database.
+     */
+
+    public static Cursor displayDatabaseInfo(HabitDbHelper mDbHelper) {
+        // Create and/or open a database to read from it
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
+        // Define a projection that specifies which columns from the database
+        // you will actually use after this query.
+        String[] projection = {
+                HabitEntry._ID,
+                HabitEntry.COLUMN_HABIT,
+                HabitEntry.COLUMN_COUNT,
+                HabitEntry.COLUMN_DAYS_TRACKED,
+                HabitEntry.COLUMN_TIMES_PER_DAY};
+
+        // Perform a query on the pets table
+        return db.query(
+                TABLE_NAME,   // The table to query
+                projection,            // The columns to return
+                null,                  // The columns for the WHERE clause
+                null,                  // The values for the WHERE clause
+                null,                  // Don't group the rows
+                null,                  // Don't filter by row groups
+                null);                   // The sort order
     }
 
     /**
@@ -58,4 +128,5 @@ public class HabitDbHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // The database is still at version 1, so there's nothing to do be done here.
     }
+
 }
